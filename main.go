f948aaf2818056db344abb745f66816bd08e27b5
Jie Burillo <benjieburillo10@gmail.com>
2024-01-21 06:00:47 +0800

    write comment

--- main.go
+++ main.go
@@ -1,5 +1,8 @@
 package main
 
+// This is a simple Go application
+// It imports fmt package for standard I/O operations
+// The main function prints "Hello World" when the application is run
 import "fmt"
 
 func main() {
