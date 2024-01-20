301cee791ca4ed2d159f577cb2db72d809ca5d24
Jie Burillo <benjieburillo10@gmail.com>
2024-01-21 06:09:44 +0800

    Refactor code and improve comments in variables.go
    The commit improves the explanatory comments in variables.go file, making them clearer and more concise. Global and local variables, and constant declaration, as well as the main function, are now annotated with more informative comments about their purpose, use, and scope.

--- variables.go
+++ variables.go
@@ -1,20 +1,26 @@
+// Declare the package `main` is in. This is the entry point for the program execution
 package main
 
+// Import required package fmt, which includes functions for printing output to console among others
 import "fmt"
 
-// Global Variables
-var globalVar = "This is global var"
+// Declare a global variable `globalVar` of type string, accessible throughout this package
+var globalVar = "This is a global variable"
 
-// Constant
+// Declare a constant `name` of type string, which cannot be changed after declaration
 const name = "Hello world"
 
+// Define the main function. This function gets executed when the program runs.
 func main() {
-	// Local Variables
-	version := 1 // infer int
+	// Declare a local variable `version` of type integer. Its scope is limited to the `main` function
+	version := 1 // type is inferred as int
 
-	// print local var
+	// Print the local variable `version` on the console
 	fmt.Println(version)
 
-	// print global var
+	// Print the constant variable `name` on the console
+	fmt.Println(name)
+
+	// Print the global variable `globalVar` on the console
 	fmt.Println(globalVar)
 }
